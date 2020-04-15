f8bc6c9f79c5cd54ca30e39bc06b0c1ebbb561e2
Andrei Scutariu <andrei.scutariu@endava.com>
2020-04-15 18:14:15 +0300

    initial commit

---
+++ SimpleGameDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleGameDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
